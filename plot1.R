4961064236b18698a1f11e98f418e4a7b7b11436
Olivier Titaud <olivier.titaud@cerfacs.fr>
2014-05-11 00:04:31 +0200

    adding data.txt and adapt scripts correspondingly

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 rm(list=ls())
 par(bg="grey")
-data<-read.table("../data.txt",sep=";",header=TRUE,dec='.',stringsAsFactors=FALSE,na.strings="?")
+data<-read.table("data.txt",sep=";",header=TRUE,dec='.',stringsAsFactors=FALSE,na.strings="?")
 png(filename = "plot1.png",
     width = 480, height = 480, units = "px", pointsize = 12,
     bg = "grey",  res = NA)
--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 rm(list=ls())
 par(bg="grey")
-data<-read.table("../data.txt",sep=";",header=TRUE,dec='.',stringsAsFactors=FALSE,na.strings="?")
+data<-read.table("data.txt",sep=";",header=TRUE,dec='.',stringsAsFactors=FALSE,na.strings="?")
 myformat<-"%d/%m/%Y %H:%M:%S"
 
 png(filename = "plot2.png", width = 480, height = 480, units = "px", pointsize = 12,    bg = "grey",  res = NA)
