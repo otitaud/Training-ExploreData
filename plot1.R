85775c1f41ce1db6d7d782350debf3d7ef8fd448
Olivier Titaud <olivier.titaud@cerfacs.fr>
2014-05-11 00:12:38 +0200

    setting par() after sewqlecting device

--- plot1.R
+++ plot1.R
@@ -1,8 +1,8 @@
 rm(list=ls())
-par(bg="grey")
 data<-read.table("data.txt",sep=";",header=TRUE,dec='.',stringsAsFactors=FALSE,na.strings="?")
 png(filename = "plot1.png",
     width = 480, height = 480, units = "px", pointsize = 12,
     bg = "grey",  res = NA)
+par(bg="grey")
 hist(as.numeric(data$Global_active_power),xlab="Global Active Power (kilowatts)",main="Global Active Power",nclass=12,col=2)
 dev.off()
--- plot3.R
+++ plot3.R
@@ -1,9 +1,10 @@
 rm(list=ls())
-par(bg="grey")
+
 data<-read.table("data.txt",sep=";",header=TRUE,dec='.',stringsAsFactors=FALSE,na.strings="?")
 myformat<-"%d/%m/%Y %H:%M:%S"
 
 png(filename = "plot3.png", width = 480, height = 480, units = "px", pointsize = 12,    bg = "grey",  res = NA)
+par(bg="grey")
 plot(strptime(paste(data$Date,data$Time),format=myformat),as.double(data$Sub_metering_1),type="l",
      main="",xlab="",ylab="Energy sub metering",,col="black")
 lines(strptime(paste(data$Date,data$Time),format=myformat),as.double(data$Sub_metering_2),col="red")
